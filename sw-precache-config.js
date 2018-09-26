40bc38607ffbca20c27064147d2e534d29deb9be
Giorgio Garasto <giorgio@garasto.it>
2018-09-26 21:13:58 +0200

    feature: Make the website actually buildable

---
+++ sw-precache-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  staticFileGlobs: [
+    'assets/**/*',
+    'src/**/*',
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: /\/@webcomponents\/webcomponentsjs\//,
+      handler: 'fastest'
+    },
+    {
+      urlPattern: /^https:\/\/fonts.gstatic.com\//,
+      handler: 'fastest'
+    }
+  ]
+};
